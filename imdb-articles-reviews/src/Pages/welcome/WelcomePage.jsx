2373f1407322d3eceb6150aeb01c4f9f5b988a1f
Almog Skalka <almogsk14@gmail.com>
2023-08-23 14:48:28 +0300

    Update WelcomePage.jsx
    Added login and signup forms. next will be handling the form data

--- imdb-articles-reviews/src/Pages/welcome/WelcomePage.jsx
+++ imdb-articles-reviews/src/Pages/welcome/WelcomePage.jsx
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import * as FaIcons from "react-icons/fa";
 
-export default function welcome() {
+export default function WelcomePage() {
+  const [ShowPassword, setShowPassword] = useState(false);
+
+  const [ShowRegistrationPassword, setShowRegistrationPassword] =
+    useState(false);
+
+  //show signin password
+  const togglePassword = () => {
+    setShowPassword((prevState) => !prevState);
+  };
+
+  //show signup password
+  const toggleRegistrationPassword = () => {
+    setShowRegistrationPassword((prevState) => !prevState);
+  };
   return (
     <div className="container">
       <div className="side-div">
@@ -16,11 +31,53 @@ export default function welcome() {
         </button>
       </div>
       {/* Login & Signup forms */}
+      {/* Login Form */}
       <div className="log-in-form">
         <h1>Login</h1>
-        <input type="text" className="text-input" placeholder="Username" />
+        <input
+          type="text"
+          className="text-input"
+          placeholder="email@email.com"
+        />
         <br />
-        <input type="password" className="text-input" placeholder="Password" />
+        <br />
+        <input
+          type={ShowPassword ? "text" : "password"}
+          className="text-input"
+          placeholder="Password"
+        />{" "}
+        {ShowPassword ? (
+          <FaIcons.FaRegEyeSlash onClick={togglePassword} />
+        ) : (
+          <FaIcons.FaRegEye onClick={togglePassword} />
+        )}
+        <br />
+        <br />
+        <button className="log-in-btn">Login!</button>
+      </div>
+      {/* Signup Form */}
+      <div className="sign-up-form">
+        <h1>Signup</h1>
+        <input
+          type="text"
+          className="text-input"
+          placeholder="email@email.com"
+        />
+        <br />
+        <br />
+        <input
+          type={ShowRegistrationPassword ? "text" : "password"}
+          className="text-input"
+          placeholder="Password"
+        />{" "}
+        {ShowRegistrationPassword ? (
+          <FaIcons.FaRegEyeSlash onClick={toggleRegistrationPassword} />
+        ) : (
+          <FaIcons.FaRegEye onClick={toggleRegistrationPassword} />
+        )}
+        <br />
+        <br />
+        <button className="sign-up-btn">Sign up!</button>
       </div>
     </div>
   );
